fc76c28c15f34fa8d4e72f7bd9e2f21b377b0448
javabrown <getrk@yahoo.com>
2012-11-30 10:29:04 -0500

    missing test file

---
+++ src/com/core/jbrown/test/Test.java
@@ -0,0 +1,10 @@
+package com.core.jbrown.test;
+
+import com.core.jbrown.data.CountryData;
+
+public class Test {
+ public static void main(String[] xx){
+	 new CountryData();
+	 System.out.println("Done");
+ }
+}
